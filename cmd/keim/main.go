74578a940ec54cc74f2ec440f86db590f70c8b34
petox05 <alberto.sosa@copixil.com>
2026-07-08 23:43:48 -0600

    chore(main): wire validator, templates and generator for manual testing

--- cmd/keim/main.go
+++ cmd/keim/main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+	/* "path/filepath" */
 
+	"keim/internal/project"
 	"keim/internal/validator"
+	"keim/internal/templates"
+	"keim/internal/generator"
 )
 
 func main() {
+
+	projectData := project.Project{
+		Name: 		"Templating",
+		Path: 		"./test/",
+		GoVersion: 	"1.26",
+	}
+
+	files := templates.FileNames()
+
 	fmt.Println("--- Probando mi validador en vivo ---")
 
-	if err := validator.Validate("./case"); err != nil {
-		fmt.Println("[Validator]: ", err)
+	if err := validator.Validate(projectData.Path, files); err != nil {
+		fmt.Printf("[Validator]: %v\n", err)
+		os.Exit(1)
 	}
-	
-	if err := validator.Validate("./cmd"); err != nil {
-		fmt.Println("[Validator]: ", err)
+	fmt.Println("La carpeta está limpia.")
+
+	fmt.Println("--- Iniciando prueba de templates embebidas ---")
+	fmt.Println("Arcvhivos a generar: ")
+	for _, file := range files {
+		fmt.Printf("%v\n", file)
 	}
+
+	err := generator.Generate(projectData, files)
+	if err != nil {
+		fmt.Printf("[Generator]: %v", err)
+	}
+	fmt.Printf("El projecto %v ha sido creádo en la carpeta %v.", projectData.Name, projectData.Path)
+
 }
