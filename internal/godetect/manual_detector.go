4d5bfe6d86077df77276e014648937d5c6a92d12
Petox03 <petox.somart@outlook.com>
2026-08-07 00:14:01 +0000

    refactor(godetect): invert conditional to use early return for version check

--- internal/godetect/manual_detector.go
+++ internal/godetect/manual_detector.go
@@ -35,26 +35,24 @@ var versionRegex = regexp.MustCompile(`^\d+\.\d+(\.\d+)?$`)
 
 func (md *ManualDetector) Detect() (string, error) {
 
-	if md.Version == "" {
-
-		res, err := prompt.String(prompt.StringOptions{
-			Stdin:        md.stdin,
-			Stdout:       md.stdout,
-			Question:     "Ingresa la versión:",
-			ErrorMessage: "Versión inválida. Intenta de nuevo.",
-			MaxRetries:   2,
-			Validate:     func(v string) bool {
-				return versionRegex.Match([]byte(v))
-			},
-		})
-
-		if err != nil {
-			return "", fmt.Errorf("manual detector: %w", err)
-		}
-
-		return res, nil
-
+	if md.Version != "" {
+		return md.Version, nil
 	}
 
-	return md.Version, nil
+	res, err := prompt.String(prompt.StringOptions{
+		Stdin:        md.stdin,
+		Stdout:       md.stdout,
+		Question:     "Ingresa la versión:",
+		ErrorMessage: "Versión inválida. Intenta de nuevo.",
+		MaxRetries:   2,
+		Validate: func(v string) bool {
+			return versionRegex.Match([]byte(v))
+		},
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("manual detector: %w", err)
+	}
+
+	return res, nil
 }
